365e70bdfd7f3eebdc27e2d3adc2ede9872d20ff
AizenLM-PC <diamanteludwing145@gmail.com>
2025-09-23 22:37:33 -0600

    notificaciones de chat funcionando

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5174",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -36,7 +36,7 @@ const io = new Server(server, {
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5174",
     credentials: true,
   })
 );
